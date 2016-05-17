fae95d0c8814cb985c433b17b921bfb025c46e7d
Pratik Panchal <pratikpanchal1808@gmail.com>
2016-05-17 17:39:34 -0400

    Score Mechanics
    Added high score into the game.

--- SurvivalshooterMod/Assets/Scripts/Managers/ScoreManager.cs
+++ SurvivalshooterMod/Assets/Scripts/Managers/ScoreManager.cs
@@ -5,22 +5,35 @@ using System.Collections;
 public class ScoreManager : MonoBehaviour
 {
     public static int score;
-
+    public static int highscore;
+    string highscorekey = "highscore";
 
     Text text;
-
-
+    
     void Awake ()
     {
-        //Get text 
+        //Get text from UI and set up variables for high score
         text = GetComponent <Text> ();
         score = 0;
+        highscore = PlayerPrefs.GetInt(highscorekey,0);
     }
 
 
     void Update ()
     {
-        //Update score
-        text.text = "Score: " + score;
+        //Update score & highscore
+        text.text = "Score: " + score + "        HighScore: " + highscore;
     }
+
+    void OnDisable()
+    {
+        //If our scoree is greter than highscore, set new higscore and save.
+        if (score > highscore)
+        {
+            PlayerPrefs.SetInt(highscorekey, score);
+            PlayerPrefs.Save();
+        }
+    }
+    
 }
+
